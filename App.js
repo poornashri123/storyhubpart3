8e1b9ea879ccab3d5cd18bc287d8ec89a16937c7
poornashri123 <59368796+poornashri123@users.noreply.github.com>
2020-10-31 20:51:46 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+
+createCanvas(400,400)
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import WriteScreen from './screens/WriteStoryScreen';
+import ReadStory from './screens/ReadStoryScreen';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      
+        <AppContainer />
+      
+    );
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Write: {screen: WriteScreen},
+  Read: {screen: ReadStory},
+});
+
+const AppContainer =  createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
